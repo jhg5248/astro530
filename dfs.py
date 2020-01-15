6c4fdf4de5605c2b21c285ebce70a28e04969015
James Gurian <jhg5248@psu.edu>
2020-01-14 21:06:34 -0500

    first commit

---
+++ dfs.py
@@ -0,0 +1,5 @@
+"""Do astro 530 things"""
+import numpy as np
+import scipy.constants as sc
+def planck(v):
+    print("a")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from ipynb.setup import find_packages
+from setuptools import setup
+
+setup(names='astro530', 
+      version='0',
+      author='James Gurian',
+      packages=find_packages(),
+      install_requires=['numpy', 'scipy', 'import-ipynb'])
