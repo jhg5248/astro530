414d9c296c3e9ab3567b1abcfc591056ffbdb69b
James Gurian <jhg5248@psu.edu>
2020-01-21 12:06:05 -0500

    added constants, math_tools to repo

---
+++ constants.py
@@ -0,0 +1,13 @@
+"""A list of physical constants, in cgs. Values mostly taken from astro.wisc.edu/~dolan/constants.html. To be expanded as necessary. We would like to avoid the overhead of Astropy, which includes unit functionality that we don't care about."""
+
+#speed of light, cm/s
+c = 29979245800.0
+
+#planck constant, erg s
+h = 6.62607015e-27
+
+#boltzmann constant
+k = 1.380649e-16
+
+#sb, erg cm^-3 K^-4
+a = 7.5657e-15
